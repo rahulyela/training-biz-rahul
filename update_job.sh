22fee554b9e96348f8a400ef4223e514f1825f30
Cloud User <ec2-user@ip-172-31-19-207.eu-north-1.compute.internal>
2024-09-26 07:02:01 +0000

    cron job 2024-09-26 07:02:01

--- update_job.sh
+++
@@ -1,5 +0,0 @@
-echo "rahul" >> /home/ec2-user/training-biz-rahul/commandslist.txt
-cd /home/ec2-user/training-biz-rahul
-git add .
-git commit -m "cron job $(date '+%Y-%m-%d %H:%M:%S')"
-git push -u origin main
